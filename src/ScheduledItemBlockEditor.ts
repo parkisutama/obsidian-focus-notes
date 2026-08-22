0a94a3d4f99cf4d06542143e7071eb01a63a5a14
Parkis Utama <49853923+parkisutama@users.noreply.github.com>
2026-08-23 06:44:52 +0700

    refactor: remove ScheduledItemBlockEditor root shim

--- src/ScheduledItemBlockEditor.ts
+++
@@ -1,8 +0,0 @@
-export {
-    parseScheduledItemBlock,
-    replaceScheduledItemBlock,
-    type ParseScheduledItemBlockResult,
-    type ScheduledItemBlock,
-    type ScheduledItemBlockDetail,
-    type ScheduledItemBlockEdit,
-} from "./features/capture/scheduled-item/domain/ScheduledItemBlockEditor.ts";
