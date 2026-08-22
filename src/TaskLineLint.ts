87869601cbc6dfd07a886e66ad0ff6c341649499
Parkis Utama <49853923+parkisutama@users.noreply.github.com>
2026-08-23 06:28:36 +0700

    refactor: remove Task line editor root shims

--- src/TaskLineLint.ts
+++
@@ -1,6 +0,0 @@
-export {
-    inspectTaskLine,
-    taskLineLintLabel,
-    type TaskLineInspection,
-    type TaskLineLintStatus,
-} from "./features/capture/scheduled-item/domain/TaskLineLint.ts";
