02619b2f685be1515dd4a7da6e9dfe856d2b72eb
Parkis Utama <49853923+parkisutama@users.noreply.github.com>
2026-08-24 10:11:07 +0700

    refactor: remove EventTaskSubmission root shim

--- src/EventTaskSubmission.ts
+++
@@ -1,8 +0,0 @@
-export {
-    retryRelatedSubmission,
-    submitInbox,
-    type EventTaskSubmissionResult,
-    type InboxSubmissionDependencies,
-    type PartialSubmissionResult,
-    type SubmissionCreatedNotes,
-} from "./features/capture/moment/application/EventTaskSubmission.ts";
